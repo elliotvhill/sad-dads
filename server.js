aa74e7675df10e29bfedcac691a53797a66929d2
Elliot <ellthrowaway2@icloud.com>
2023-08-13 15:43:14 -0400

    created paths for all artists, concerts, venues, and by ID

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express')
 const cors = require('cors')
 const PORT = process.env.MONGOPORT || 3001
 const db = require('./db/db')
-const { Artist } = require('./models/index')
+const { Artist, Concert, Venue } = require('./models/index')
 
 const app = express()
 
@@ -15,6 +15,29 @@ app.get('/', (req, res) => {
 app.get('/artists', async (req, res) => {
     const artists = await Artist.find({})
     res.json(artists)
+})
+app.get('/artists/:id', async (req, res) => {
+    const { id } = req.params
+    const artist = await Artist.findById(id)
+    res.json(artist)
+})
+app.get('/concerts', async (req, res) => {
+    const concerts = await Concert.find({})
+    res.json(concerts)
+})
+app.get('/concerts/:id', async (req, res) => {
+    const { id } = req.params
+    const concert = await Concert.findById(id)
+    res.json(concert)
+})
+app.get('/venues', async (req, res) => {
+    const venues = await Venue.find({})
+    res.json(venues)
+})
+app.get('/venues/:id', async (req, res) => {
+    const { id } = req.params
+    const venue = await Venue.findById(id)
+    res.json(venue)
 })
 
 app.listen(PORT, () => {
