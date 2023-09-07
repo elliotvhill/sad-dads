b032a2e105566d9d3e9891b545c716fea74881eb
Elliot <ellthrowaway2@icloud.com>
2023-09-07 08:30:23 -0400

    reverted concert model lineup data type

--- models/concert.js
+++ models/concert.js
@@ -2,8 +2,7 @@ const { Schema } = require('mongoose')
 const concertSchema = new Schema(
     {
         concert_day: { type: String },
-        lineup: [{ type: Schema.Types.ObjectId, ref: 'Artist' }],
-        // lineup: { type: Array },
+        lineup: { type: Array },
         attendees: { type: Number },
         ticket_price: { type: Number },
         venue_id: { type: Schema.Types.ObjectId, ref: 'Venue' },
